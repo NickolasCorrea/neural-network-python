167121cb2cd515dd78de2dad0a36d4dd6d1de520
Nickolas de Souza Silveira Corrêa <nickolassscorrea@gmail.com>
2023-09-12 23:23:47 -0300

    Ajustes nas entradas do perceptron

--- perceptron.py
+++ perceptron.py
@@ -1,11 +1,6 @@
 # NICKOLAS DE SOUZA SILVEIRA CORRÊA - RA: 185823
 # VICTOR RICO MOURA SANTOS - RA: 191068
 
-NRCASOS = 8  # verificar depois o que o define faz
-
-w1 = 87
-w2 = 92
-
 def funcao_ativacao(x1, x2):
     net = x1*w1 + x2*w2
     y = -1.0
@@ -13,26 +8,32 @@ def funcao_ativacao(x1, x2):
         y = 1.0
     return y
 
+
+NRCASOS = 8
+w1 = 85
+w2 = 75
 i = 0
 passo = 0.0009  # constante de aprendizado
 
-x1 = [25.0, 22.0, 30.0, 27.0, 4.0, 6.0, 10.0, 3.0]
-x2 = [34.0, 37.0, 33.0, 37.0, 40.0, 38.0, 40.0, 42.0]
-t = [-1.0, -1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0]  # target
+x1 = [25, 22, 30, 27, 4, 6, 10, 3]
+x2 = [34, 37, 33, 37, 40, 38, 44, 42]
+t = [-1, -1, -1, -1, 1, 1, 1, 1]  # target
 
 nr_acertos = 0  # acertos por treinamento
 nr_treinamentos = 0
 erro1 = 0.0
 erro2 = 0.0
 
+
 while (1):
+
     nr_treinamentos += 1
     nr_acertos = 0
     erro1 = 0.0
     erro2 = 0.0
 
     for i in range(NRCASOS):  # treinamento
-        
+
         y = funcao_ativacao(x1[i], x2[i])
         if (y == t[i]):  # comparando y com o target
             nr_acertos += 1
@@ -45,6 +46,7 @@ while (1):
 
     if (nr_acertos == NRCASOS):  # convergiu
         break
+
     else:  # atualizando os pesos
         w1 += passo * erro1
         w2 += passo * erro2
