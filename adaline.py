c909f1b020f746a1e3430b4c34c8999e2cb28515
Nickolas de Souza Silveira Corrêa <nickolassscorrea@gmail.com>
2023-10-08 23:45:29 -0300

    Corrigindo nomes das variaveis e adicionando calculo do novo peso 0

--- adaline.py
+++ adaline.py
@@ -2,44 +2,52 @@
 #VICTOR RICO MOURA SANTOS - RA: 191068
 
 def funcao_ativacao(x1, x2):
-    net = x1*w1 + x2*w2
-    y = -1.0
-    if (net >= 0.0):
-        y = 1.0
-    return y
+    saidaIntermediaria = (x0*w0) + (x1*w1) + (x2*w2)
+    saida = -1.0
+    if (saidaIntermediaria >= 0.0):
+        saida = 1.0
+    return saidaIntermediaria, saida
 
-NRCASOS = 8  #verificar depois o que o define faz
-w1 = 85
-w2 = 75
-i = 0
-taxa = 0.0009  # constante de aprendizado
 
-x1 = [25,22,30,27,4,6,10,3]
-x2 = [34,37,33,37,40,38,44,42]
-t = [-1,-1,-1,-1,1,1,1,1]  #target
+# x1 = [25, 22, 30, 27, 4, 6, 10, 3]
+# x2 = [34, 37, 33, 37, 40, 38, 44, 42]
+# target = [-1, -1, -1, -1, 1, 1, 1, 1]  #target
+NRCASOS = 2
+x1 = [30, 10]
+x2 = [33, 44]
+target = [-1, 1]  #target
 
-nr_acertos = 0  #acertos por treinamento
-nr_treinamentos = 0
+x0 = 1
+w0 = 0
+w1 = 0.02
+w2 = 0.01
+
+taxaAprendizado = 0.0001  # constante de aprendizado (0 < taxaAprendizado < 2 )
+nrAcertos = 0  # acertos por treinamento
+nrTreinamentos = 0
 erro = 0
 
 while (1):
-    nr_treinamentos += 1
-    nr_acertos = 0
-    
-    for i in range(NRCASOS):
-        
-        y = funcao_ativacao(x1[i], x2[i])
-        if (y == t[i]):  # comparando y com o target
-            nr_acertos += 1
-            print(i)
-        erro = t[i] - y
-    
-    print("Treino: " + str(nr_treinamentos) + ", acertos: " + str(nr_acertos) + "\n")
+    nrTreinamentos += 1
+    nrAcertos = 0
 
-    if (nr_acertos == NRCASOS):  #convergiu
+    contador = 0
+    for contador in range(NRCASOS):
+        
+        saidaIntermediaria, saida = funcao_ativacao(x1[contador], x2[contador])
+        if (saida == target[contador]):
+            nrAcertos += 1
+            print("Contador: " + str(contador))
+        erro = target[contador] - saidaIntermediaria
+    
+    print("Treino: " + str(nrTreinamentos) + ", acertos: " + str(nrAcertos) + "\n")
+    print("Saida Intermediaria: " + str(saidaIntermediaria))
+
+    if (nrAcertos == NRCASOS):  # convergiu
         break
     else:
-        w1 += taxa * erro * x1[i] / (abs(x1[i])**2)
-        w2 += taxa * erro * x1[i] / (abs(x1[i])**2)
+        w0 += taxaAprendizado * erro * x0 / (abs(x0)**2)
+        w1 += taxaAprendizado * erro * x1[contador] / (abs(x1[contador])**2)
+        w2 += taxaAprendizado * erro * x2[contador] / (abs(x2[contador])**2)
 
 print("Pesos Finais: w1: " + str(w1) + ", w2: " + str(w2) + ", \n")
