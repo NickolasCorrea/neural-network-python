f912c135025aed432ba7c41f5bece7cff52be719
ricoVictor <victorrico.2002@yahoo.com.br>
2023-10-11 23:44:27 -0300

    Removido modulo no calculo do erro e prints de variaveis para debug

--- adaline.py
+++ adaline.py
@@ -5,15 +5,6 @@ from math import sqrt
 
 def funcao_ativacao(x1, x2):
     saidaIntermediaria = (x[0]*w[0]) + (x1*w[1]) + (x2*w[2])
-    print("x0: " + str(x[0]))
-    print("w0: " + str(w[0]))
-    print("x0*w0: " + str(x[0]*w[0]))
-    print("x1: " + str(x1))
-    print("w1: " + str(w[1]))
-    print("x1*w1: " + str(x1*w[1]))
-    print("x2: " + str(x2))
-    print("w2: " + str(w[2]))
-    print("x2*w2: " + str(x2*w[2]))
     saida = -1.0
     if (saidaIntermediaria >= 0.0):
         saida = 1.0
@@ -26,17 +17,23 @@ def funcao_ativacao(x1, x2):
 # x1 = [2]
 # x2 = [31]
 
-NRCASOS = 8
+'''NRCASOS = 16  # mistureba do Nichollas
+taxaAprendizado = 0.01  # constante de aprendizado (0 < taxaAprendizado < 2)
+x = [1, [25, 22, 30, 27, 35, 38, 26, 33, 4, 6, 10, 3, 4, 15, 10, 7], [34, 37, 33, 37, 34, 39, 35, 37, 40, 38, 44, 42, 46, 45, 38, 48]]
+w = [0, 4, -12]
+target = [-1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1]  #target'''
+
+NRCASOS = 8  # cabelo sapato
 taxaAprendizado = 0.0001  # constante de aprendizado (0 < taxaAprendizado < 2)
 x = [1, [25, 22, 30, 27, 4, 6, 10, 3], [34, 37, 33, 37, 40, 38, 44, 42]]
 w = [0, 0.02, 0.01]
 target = [-1, -1, -1, -1, 1, 1, 1, 1]  #target
 
-# NRCASOS = 1
-# taxaAprendizado = 0.01  # constante de aprendizado (0 < taxaAprendizado < 2)
-# x = [1, [2], [31]]
-# w = [0, 4, -12]
-# target = [1]  #target
+'''NRCASOS = 1  # documento do Nichollas
+taxaAprendizado = 0.01  # constante de aprendizado (0 < taxaAprendizado < 2)
+x = [1, [2], [31]]
+w = [0, 4, -12]
+target = [1]  #target'''
 
 # x0 = 1
 # w0 = 0
@@ -47,7 +44,7 @@ nrAcertos = 0  # acertos por treinamento
 nrTreinamentos = 0
 erro = 0
 
-while (nrAcertos != NRCASOS and nrTreinamentos < 1):
+while (nrAcertos != NRCASOS):
     nrTreinamentos += 1
     nrAcertos = 0
 
@@ -63,7 +60,7 @@ while (nrAcertos != NRCASOS and nrTreinamentos < 1):
         if (nrAcertos == NRCASOS):  # convergiu
             break
         else:
-            erro = target[contador] - abs(saidaIntermediaria)
+            erro = target[contador] - saidaIntermediaria
             # xQuadrado = (x[0]**2 + x[1][contador]**2 + x[2][contador]**2)
             xQuadrado = x[0]**2
             for num in range(1, len(x)):
@@ -72,10 +69,5 @@ while (nrAcertos != NRCASOS and nrTreinamentos < 1):
             w[0] += taxaAprendizado * erro * x[0] / xQuadrado
             w[1] += taxaAprendizado * erro * x[1][contador] / xQuadrado
             w[2] += taxaAprendizado * erro * x[2][contador] / xQuadrado
-            print("w0: " + str(w[0]))
-            print("w1: " + str(w[1]))
-            print("w2: " + str(w[2]))
-            print("Erro: " + str(erro))
-            print("Saida Intermediária: " + str(saidaIntermediaria) + "\n")
 
-print("Pesos Finais: w1: " + str(w[1]) + ", w2: " + str(w[2]) + ", w0: " + str(w[0]) + ", \n")
+print("Pesos Finais: w1: " + str(w[1]) + ", w2: " + str(w[2]) + "\n")
