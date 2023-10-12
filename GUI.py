12c421b3a6a3588f80d414bd23af10b1ab17132e
Nickolas de Souza Silveira Corrêa <nickolassscorrea@gmail.com>
2023-10-12 00:16:08 -0300

    adicionando frame de casos na GUI

--- GUI.py
+++ GUI.py
@@ -14,14 +14,14 @@ pesos_frame = tkinter.LabelFrame(frame, text = "Insira os pesos iniciais")
 pesos_frame.grid(row = 0, column = 0, padx = 20, pady = 20)
 
 # Criando label para o peso1
-peso1_label = tkinter.Label(pesos_frame, text= "W1: ")
+peso1_label = tkinter.Label(pesos_frame, text= "W1:")
 peso1_label.grid(row = 0, column = 0)
 # Criando entrada para o peso1
 peso1_entrada = tkinter.Entry(pesos_frame)
 peso1_entrada.grid(row = 0, column = 1)
 
 # Criando label para o peso2
-peso2_label = tkinter.Label(pesos_frame, text= "W2: ")
+peso2_label = tkinter.Label(pesos_frame, text= "W2:")
 peso2_label.grid(row = 1, column = 0)
 # Criando entrada para o peso2
 peso2_entrada = tkinter.Entry(pesos_frame)
@@ -38,14 +38,14 @@ bias_frame = tkinter.LabelFrame(frame, text = "Bias")
 bias_frame.grid(row = 0, column = 1, padx = 20, pady = 20)
 
 # Criando label para o X0
-x0_label = tkinter.Label(bias_frame, text= "X0: ")
+x0_label = tkinter.Label(bias_frame, text= "X0:")
 x0_label.grid(row = 0, column = 0)
 # Criando entrada para o X0
 x0_entrada = tkinter.Entry(bias_frame)
 x0_entrada.grid(row = 0, column = 1)
 
 # Criando label para o peso0
-peso0_label = tkinter.Label(bias_frame, text= "W0: ")
+peso0_label = tkinter.Label(bias_frame, text= "W0:")
 peso0_label.grid(row = 1, column = 0)
 # Criando entrada para o peso0
 peso0_entrada = tkinter.Entry(bias_frame)
@@ -79,6 +79,32 @@ numMaxTreinos_entrada.grid(row = 1, column = 1)
 for widget in treinoNeuronio_frame.winfo_children():
     widget.grid_configure(padx = 5, pady = 5)
 
+#-------------------------------------------------------------------------
+
+# Criando Grid para os CASOS
+casos_frame = tkinter.LabelFrame(frame, text = "Casos")
+casos_frame.grid(row = 1, column = 0, padx = 20, pady = 20)
+
+# Criando label para o X1
+x1_label = tkinter.Label(casos_frame, text= "X1:")
+x1_label.grid(row = 0, column = 0)
+# Criando entrada para o X1
+x1_entrada = tkinter.Entry(casos_frame)
+x1_entrada.grid(row = 0, column = 1)
+
+# Criando label para o X2
+x2_label = tkinter.Label(casos_frame, text= "X2:")
+x2_label.grid(row = 0, column = 2)
+# Criando entrada para o X2
+x2_entrada = tkinter.Entry(casos_frame)
+x2_entrada.grid(row = 0, column = 3)
+
+# Cirando um padding para todos os conteudos dentro do frame casos_frame
+for widget in casos_frame.winfo_children():
+    widget.grid_configure(padx = 5, pady = 5)
+
+button = tkinter.Button(casos_frame, text = "Salvar")
+button.grid(row = 0, column = 4)
 
 
 # Criando uma comboBox
